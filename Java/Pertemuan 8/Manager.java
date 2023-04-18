d1a4c31fdfd239fe8615321b1717e8422000a39d
Tantowi Shah <114087993+tantowish@users.noreply.github.com>
2023-04-18 17:32:40 +0700

    Add files via upload

---
+++ Java/Pertemuan 8/Manager.java
@@ -0,0 +1,56 @@
+package Overriding2;
+
+import java.util.List;
+
+public class Manager extends Pegawai{
+    private List<Pegawai> anakBuah;
+    private Pegawai sekretaris;
+    private String Ruangan;
+
+    public List<Pegawai> getAnakBuah() {
+        return anakBuah;
+    }
+
+    public void setAnakBuah(List<Pegawai> anakBuah) {
+        this.anakBuah = anakBuah;
+    }
+
+    public Pegawai getSekretaris() {
+        return sekretaris;
+    }
+
+    public void setSekretaris(Pegawai sekretaris) {
+        this.sekretaris = sekretaris;
+    }
+    public void setSekretaris(String nip, String nama){
+        Pegawai p = new Pegawai();
+        p.setNama(nama);
+        p.setNip(nip);
+    }
+
+    public String getRuangan() {
+        return Ruangan;
+    }
+
+    public void setRuangan(String ruangan) {
+        Ruangan = ruangan;
+    }
+
+    @Override
+    public int getThr() {
+        return super.getGaji()*2;
+    }
+    @Override
+    public void setNip(String nip) {
+        if(nip.charAt(0)=='M') {
+            super.setNip(nip);;
+        }
+        else{
+            System.out.println("Gagal");
+        }
+    }
+    @Override
+    public void setNip(int urutan) {
+        this.setNip((String) "M"+urutan);
+    }
+}
---
+++ Java/Pertemuan 8/Pegawai.java
@@ -0,0 +1,100 @@
+package Overriding2;
+
+public class Pegawai {
+    private String nama, nip, departement,posisi;
+    private int gaji;
+    private int cuti=12;
+    private boolean isMale;
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNip() {
+        return nip;
+    }
+
+    public String getDepartement() {
+        return departement;
+    }
+
+    public void setDepartement(String departement) {
+        this.departement = departement;
+    }
+
+    public String getPosisi() {
+        return posisi;
+    }
+
+    public void setPosisi(String posisi) {
+        this.posisi = posisi;
+    }
+
+    public int getGaji() {
+        return gaji;
+    }
+
+    public void setGaji(int gaji) {
+        this.gaji = gaji;
+    }
+    public void setGaji(int gaji,int tunjangan){
+
+        this.gaji=gaji+tunjangan;
+    }
+    public void setGaji(int gaji,int tunjangan,int potongan){
+        this.gaji=gaji+tunjangan;
+    }
+    public int getThr(){
+        return this.getGaji();
+    }
+
+    public int getCuti() {
+        return cuti;
+    }
+
+    public void setCuti(int cuti) {
+        this.cuti += cuti;
+    }
+    public void setCuti(String tipeCuti) {
+        if(tipeCuti.equalsIgnoreCase("pernikahan")){
+            this.cuti+=2;
+        }
+        else if(tipeCuti.equalsIgnoreCase("persalinan")){
+            if (isMale){
+                this.cuti+=3;
+            }
+            else{
+                this.cuti+=90;
+            }
+
+        }
+    }
+    public boolean isMale() {
+        return isMale;
+    }
+
+    public void setMale(boolean male) {
+        isMale = male;
+    }
+
+    public void setNip(String nip) {
+        if(nip.charAt(0)=='P') {
+            this.nip = nip;
+        }
+        else if(nip.charAt(0)=='S'){
+            this.nip = nip;
+        }
+        else if(nip.charAt(0)=='M'){
+            this.nip = nip;
+        }
+        else{
+            System.out.println("Gagal");
+        }
+    }
+    public void setNip(int urutan){
+        this.nip="P"+urutan;
+    }
+}
